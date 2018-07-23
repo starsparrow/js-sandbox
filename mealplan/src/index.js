27596aa7cfcb896e95f5f9f3bb846291fc9e4b09
starsparrow <nik.spurgetis@gmail.com>
2018-07-23 12:42:04 -0400

    Moved mealplan data to separate file. Deleted old projects.

--- mealplan/src/index.js
+++ mealplan/src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
-
-
-const data = [
-  {
-    title: "Spicy Pork"
-  },
-  {
-    title: "Baked Potatoes"
-  },
-  {
-    title: "Breakfast in the Night"
-  },
-  {
-    title: "Taco Goop"
-  },
-  { title: "Omurice" },
-  { title: "Carne Asada Tacos" },
-  { title: "Tuna Casserole" },
-  { title: "Beef Stroganoff" },
-  { title: "Parsi Chicken" },
-]
-
+import data from './data.json';
 
 function getRandomMeals(n) {
   let meals = [];
@@ -39,7 +18,7 @@ function getRandomMeals(n) {
 function Card(props) {
   return (
     <div className="wrapper">
-      <div className="card">
+      <div onClick={props.onClick} className="card">
         <h1>{props.title}</h1>
       </div>
     </div>
@@ -47,21 +26,74 @@ function Card(props) {
 }
 
 
-function Content(props) {
+function ShoppingList(props) {
+  let ingredients = props.recipes.map(
+    function(recipe) {return recipe.ingredients} 
+  );
+  ingredients = [].concat(...ingredients); 
 
-    return(
-	    <div className="content">
-	      {
-	        props.recipes.map(
-		  function(recipe, index) {
-                    return <Card key={index} title={recipe.title} />
-                  }
-		)
-	      }
-	    </div>
-    );
+
+  return (
+    <div className="wrapper">
+      <div className="shoppinglist">
+        <p>Ingredients for current meal plan:</p>
+	<ul>
+	  {
+	    ingredients.map(
+	      function(ingredient, index) {return <li key={index}>{ingredient}</li>  }
+	    ) 
+	  }
+	</ul>
+      </div>
+    </div>
+  );
 }
 
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+	    numberOfMeals: 5,
+	    recipes: [],
+	    shoppingList: []
+    };
+  }
+
+  updateRecipes() {
+    this.setState((prevState) => {
+      return {
+	       recipes: getRandomMeals(this.state.numberOfMeals),
+	     }
+
+    });
+  }
+
+
+
+  render() {
+    if ( this.state.recipes.length === 0 ) {	  
+      this.updateRecipes();
+    }
+
+    return(
+      <div className="content">
+        {
+          this.state.recipes.map(
+	    function(recipe, index) {
+	      return <Card key={index} title={recipe.title} />
+	    }
+	  )
+        }
+	    <div className="wrapper">
+        <div className="refresh" onClick={() => this.updateRecipes(this.state.numberOfMeals)}><h1>⟳</h1></div></div>
+	<ShoppingList recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <Content recipes={getRandomMeals(5)} />, document.getElementById('root')
+  <Content />, document.getElementById('root')
 );
